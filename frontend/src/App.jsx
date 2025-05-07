c4efb94fdb4afe6d5ec4e31d712538dd1dfd6e99
hillarynve <hillaryan@g.ucla.edu>
2025-05-06 21:55:02 -0700

    cleaning up directory, delete public/vite.svg since unused.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Signup from './Signup';
 import Login from './Login';
 import Home from './Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/register" />} />
         <Route path="/register" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
